b7ead82223418c8be09d10489b3a77e9716324a3
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-10-08 13:32:24 -0600

    Update!

--- src/App.js
+++ src/App.js
@@ -11,24 +11,38 @@ function App() {
     const showParticipates = () => setList(true);
 
     return (
+        <>
         <div className="App">
             <div>
-                <button onClick={showChat}>Chat</button>
-                <button onClick={showParticipates}>Participates</button>
+                <div className='Tab-buttons'>
+                    <button className={showList ? '' : 'active'} onClick={showChat}>Chat</button>
+                    <button className={showList ? 'active' : ''} onClick={showParticipates}>Participates</button>
+                </div>
+                {(showList)
+                    ? (<ul className="App-list">
+                        {store.participants.map(item => (item.inSession) ? (
+                            <List key={item.id} name={item.name} avatar={item.avatar}
+                                  onStage={item.onStage}/>) : '')}
+                    </ul>)
+                    : (<div className="App-list">
+                        {store.chatEvents.map((item, index) => (item.message)
+                            ? (<Chat key={index} id={item.participantId} type={item.type} message={item.message}
+                                     time={item.time}/>)
+                            : <Chat key={index} id={item.participantId} type={item.type}/>)}
+                    </div>)}
             </div>
-            {(showList)
-                ? (<ul className="App-list">
-                    {store.participants.map(item => (item.inSession) ? (
-                        <List key={item.id} name={item.name} avatar={item.avatar} onStage={item.onStage}/>) : '')}
-                </ul>)
-                : (<div className="App-list">
-                    {store.chatEvents.map((item, index) => (item.message)
-                        ? (<Chat key={index} id={item.participantId} type={item.type} message={item.message}
-                                 time={item.time}/>)
-                        : <Chat key={index} id={item.participantId} type={item.type}/>)}
-                </div>)}
             <Stage key='1' store={store.participants}/>
         </div>
+        <div className='Footer'>
+            <div className='Emotes'>
+                <button> {'<'} </button>
+                <button>Up</button>
+                <button>Down</button>
+                <button>Raise Hand</button>
+                <button>Clap</button>
+            </div>
+        </div>
+        </>
     );
 }
 
