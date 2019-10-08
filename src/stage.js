e0cbf4d15d7d6a15d78601b676bfd5c24e9c2fcc
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-10-08 11:22:30 -0500

    added stage component

---
+++ src/stage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import participants from './store';
+import './stage.css';
+
+function Tile(props) {
+
+	const alt = `The avatar of ${props.name}`
+	return (
+		<div className='tile'>
+			<h2>{props.name}</h2>
+			<img src={props.avatar} alt={alt}/>
+		</div>
+	);
+}
+
+function Stage() {
+	const arr = participants.participants.map(item => {
+			if (item.onStage) return <Tile name={item.name} avatar={item.avatar} />;
+		})
+
+	return (
+		<section className='stage'>
+			{arr}
+		</section>
+	);
+}
+
+export default Stage;
