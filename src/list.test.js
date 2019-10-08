7124bc68a263d966867551a090287fd3be31a63e
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-10-08 12:26:51 -0600

    Update list.test.js

--- src/list.test.js
+++ src/list.test.js
@@ -5,22 +5,19 @@ import renderer from 'react-test-renderer';
 import List from './List';
 import store from './store';
 
-let data = store.lists.map(item => {
-	return{
-	id: item.id,
-	header: item.header,
-	cards: item.cardIds.map(item => store.allCards[item])
-}});
+let data = store.participants.map(item => {
+	return {name: item.name, avatar: item.avatar, onStage: item.onStage}
+});
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	ReactDOM.render(<List key={data[0].id} header={data[0].header} cards={data[0].cards} />, div);
+	ReactDOM.render((<List key={data[0].id} name={data[0].name} avatar={data[0].avatar} onStage={data[0].onStage}/>), div);
 	ReactDOM.unmountComponentAtNode(div);
 });
 
 it('renders the List as expected', () => {
 	const list = renderer
-		.create(<List key={data[0].id} header={data[0].header} cards={data[0].cards} />)
+		.create(<List key={data[0].id} name={data[0].name} avatar={data[0].avatar} onStage={data[0].onStage}/>)
 		.toJSON();
 	expect(list).toMatchSnapshot();
 });
